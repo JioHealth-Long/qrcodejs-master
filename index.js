8f59a2a15f942a1f0051144920b53972d7e64626
Long Nguyen <long.nguyen@jiohealth.com>
2020-03-13 12:08:03 +0700

    stype qr code

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-/**
- * Document ready
+/***
+ * qr code
  */
 var qrcode1 = new QRCode(document.getElementById("qrcode1"), {
-    width: 45,
-    height: 45
+    width: 55,
+    height: 55
 });
 var qrcode2 = new QRCode(document.getElementById("qrcode2"), {
-    width: 45,
-    height: 45
+    width: 55,
+    height: 55
 });
 var qrcode3 = new QRCode(document.getElementById("qrcode3"), {
-    width: 45,
-    height: 45
+    width: 55,
+    height: 55
 });
 var qrcode4 = new QRCode(document.getElementById("qrcode4"), {
-    width: 45,
-    height: 45
+    width: 55,
+    height: 55
 });
 
 function makeCode() {
     var stockIdEle = document.getElementById("stock");
-    let batchNumber = document.getElementById("batch-number");
+    let batchNumberEle = document.getElementById("batch-number");
 
     if (!stockIdEle.value) {
         alert("Input a text");
@@ -28,21 +28,52 @@ function makeCode() {
         return;
     }
 
-    qrcode1.makeCode(`${stockIdEle.value}*${batchNumber.value}`);
-    qrcode2.makeCode(`${stockIdEle.value}*${batchNumber.value}`);
-    qrcode3.makeCode(`${stockIdEle.value}*${batchNumber.value}`);
-    qrcode4.makeCode(`${stockIdEle.value}*${batchNumber.value}`);
+    qrcode1.makeCode(`${stockIdEle.value}*${batchNumberEle.value}`);
+    qrcode2.makeCode(`${stockIdEle.value}*${batchNumberEle.value}`);
+    qrcode3.makeCode(`${stockIdEle.value}*${batchNumberEle.value}`);
+    qrcode4.makeCode(`${stockIdEle.value}*${batchNumberEle.value}`);
 }
 
 makeCode();
 // "#stock #batch-number"
 
-
+/**
+ * Document ready
+ */
 $(document).ready(() => {
+    // get params
+    const queryString = window.location.search;
+    console.log(queryString);
+
+    const urlParams = new URLSearchParams(queryString);
+
+    const stock = urlParams.get('stock');
+    console.log(stock);
+
+    if (stock) {
+        $('#stock').val(stock);
+        $('.stock').text(stock)
+    } else {
+        $('#batch-number').val('');
+        $('.stock').text('')
+    }
+    // shirt
+
+    const batchNumber = urlParams.get('batch-number');
+    console.log(batchNumber);
+    if (batchNumber) {
+        $('#stock').val(batchNumber);
+        $('.batch-id').text(batchNumber);
+    } else {
+        $('#batch-number').val('');
+        $('.batch-id').text('');
+    }
+
     $('#print-all').on('click', () => {
         console.log('Print all');
         window.print();
     });
+
     $("#stock").on("blur", function () {
         makeCode();
     }).on("keydown", function (e) {
@@ -50,6 +81,7 @@ $(document).ready(() => {
             makeCode();
         }
     });
+
     $("#batch-number").on("blur", function () {
         makeCode();
     }).on("keydown", function (e) {
